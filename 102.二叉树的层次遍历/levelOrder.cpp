2569d12236dac5dc91efe17073d95de72f44f929
YichengZhong <1165240284@qq.com>
2019-11-28 21:28:22 +0800

    题102

---
+++ 102.二叉树的层次遍历/levelOrder.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) 
+    {
+        vector<vector<int>> vec;
+
+        if(root==NULL)
+        {
+            return vec;
+        }       
+
+        queue<TreeNode*> que;
+        que.push(root);
+
+        while(!que.empty())
+        {
+
+            int size=que.size();
+            vector<int> vec_temp;
+
+            while(size--)
+            {
+
+                TreeNode* t = que.front();
+                vec_temp.push_back(que.front()->val);
+                que.pop();    
+
+                if(t->left)
+                {
+                    que.push(t->left);
+                }
+
+                if(t->right)
+                {
+                    que.push(t->right);
+                }
+            }
+            vec.push_back(vec_temp);
+        }
+        return vec;
+    }
+};
