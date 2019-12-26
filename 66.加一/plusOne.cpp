c445aed4f271ada81fe681c22ca090e7e34a1aae
YichengZhong <1165240284@qq.com>
2019-12-26 21:53:52 +0800

    题66

---
+++ 66.加一/plusOne.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+         int n = digits.size();
+        for (int i = n - 1; i >= 0; --i)
+        {
+            if (digits[i] == 9) 
+                digits[i] = 0;
+            else
+            {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+        if (digits.front() == 0) 
+            digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
