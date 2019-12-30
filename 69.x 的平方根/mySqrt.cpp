eeae469933a8e0276f8e2a6204ea922aa3b87ac2
YichengZhong <1165240284@qq.com>
2019-12-30 22:55:03 +0800

    题69

---
+++ 69.x 的平方根/mySqrt.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long res = x;
+        while (res * res > x) {
+            res = (res + x / res) / 2;
+        }
+        return res;
+    }
+};
