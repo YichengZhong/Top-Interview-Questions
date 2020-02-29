7be62bcdcb0cc06462b6d10b6ffc0c5a9073fdf7
YichengZhong <1165240284@qq.com>
2020-02-29 22:15:19 +0800

    题35

---
+++ 35.搜索插入位置/searchInsert.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1, mid;
+    
+        while (left <= right)
+        {
+            mid = (left + right)/2;
+            if (nums[mid] > target) 
+                right = mid - 1;
+            else if (nums[mid] < target) 
+                left = mid + 1;
+            else return mid;
+        }
+   
+        return left;
+    }
+};
