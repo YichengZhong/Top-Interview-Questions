dee051505d021f2589dbe39654362fedde9b61cd
YichengZhong <1165240284@qq.com>
2020-03-22 19:38:23 +0800

    题179的修改

--- 179.最大数/largestNumber.cpp
+++ 179.最大数/largestNumber.cpp
@@ -1,3 +1,13 @@
+bool compare(string a,string b)
+{
+    if(atoll((a+b).c_str())>atoll((b+a).c_str()))
+    {
+        return true;
+    }
+    
+    return false;
+}
+
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
@@ -14,15 +24,20 @@ public:
             str_nums.push_back(to_string(nums[i]));
         }
         
-        sort(str_nums.begin(),str_nums.end());
+        sort(str_nums.begin(),str_nums.end(),compare);
         
-        reverse(str_nums.begin(),str_nums.end());
+        //reverse(str_nums.begin(),str_nums.end());
         
         string str_out="";
         
         for(int i=0;i<str_nums.size();++i)
         {
             str_out=str_out+str_nums[i];
+        }
+
+        if(atoll(str_out.c_str())==0)
+        {
+            return "0";
         }
         
         return str_out;
