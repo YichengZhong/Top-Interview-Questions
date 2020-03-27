83ecb85cbf59e19f03ee9569950398c0f3e827ec
YichengZhong <1165240284@qq.com>
2020-03-27 21:41:53 +0800

    题124

---
+++ 124.二叉树中的最大路径和/maxPathSum.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        
+    }
+
+    int pathSum(TreeNode* root, int sum) {
+        if (root==NULL) return 0;
+        return DFS(root,sum);
+        
+    }
+
+    int solve(TreeNode* curr, int target)
+    {
+        int ans=0;
+        if(curr==NULL) return 0;
+        if(curr->val == target) ans++;
+        ans+=solve(curr->left, target-curr->val);
+        ans+=solve(curr->right,target-curr->val);
+        return ans;
+    }
+
+    int DFS(TreeNode* curr, int target)
+    {
+        int ans=0;
+        if(curr==NULL) return 0;
+        ans+= solve(curr,target);
+        ans+=DFS(curr->left,target);
+        ans+=DFS(curr->right,target);
+        return ans;
+    }
+};
