4a0e56c26fa90694272fd03c483212717dc04b50
YichengZhong <1165240284@qq.com>
2020-03-30 21:55:48 +0800

    面试题10.11.峰与谷

---
+++ 面试题10.11.峰与谷/wiggleSort.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        for(int i=1;i<nums.size();i++)
+        {
+            if(i%2==0)
+            {
+                if(nums[i]<nums[i-1]) 
+                {
+                    swap(nums[i],nums[i-1]);
+                }
+                
+            }
+            else
+            {
+                if(nums[i]>nums[i-1])
+                {
+                    swap(nums[i],nums[i-1]);
+                } 
+            }
+        }
+    }
+};
