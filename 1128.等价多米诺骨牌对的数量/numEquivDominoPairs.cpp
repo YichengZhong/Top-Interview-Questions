cc933e43f0df1b7b77141a01724d7af831b6b0d0
YichengZhong <1165240284@qq.com>
2020-04-04 20:32:15 +0800

    1128.等价多米诺骨牌对的数量

---
+++ 1128.等价多米诺骨牌对的数量/numEquivDominoPairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        int count=0;
+        for(int i=0;i<dominoes.size()-1;++i)
+        {
+            for(int j=i+1;j<dominoes.size();++j)
+            {
+                if(((dominoes[i][0]==dominoes[j][0] && dominoes[i][1]==dominoes[j][1])) ||((dominoes[i][0]==dominoes[j][1] && dominoes[i][1]==dominoes[j][0])))
+                {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+        
+    }
+};
