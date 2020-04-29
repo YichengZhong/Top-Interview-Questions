8b4488a4e623ea31f2dedbbfc8dbe67f9ecec5f5
YichengZhong <1165240284@qq.com>
2020-04-29 20:38:53 +0800

    5392.分割字符串的最大得分

---
+++ 5392.分割字符串的最大得分/maxScore.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        vector<int>v_zero(s.size()-1,0);
+        vector<int>v_one(s.size()-1,0);
+
+        int temp=0;
+        for(int i=0;i<s.size()-1;++i)
+        {
+            if(s[i]=='0')
+            {
+                temp++;
+            } 
+            v_zero[i]=temp;
+        }
+
+        temp=0;
+        for(int i=s.size()-1;i>0;--i)
+        {
+            if(s[i]=='1')
+            {
+                temp++;
+            } 
+            v_one[i-1]=temp;
+        }
+
+        int max=0;
+        for(int i=0;i<s.size()-1;++i)
+        {
+            if(max<(v_zero[i]+v_one[i]))
+            {
+                max=v_zero[i]+v_one[i];
+            }
+        }
+
+        return max;
+    }
+};
