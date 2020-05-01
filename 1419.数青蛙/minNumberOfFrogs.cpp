3243cd0391e04a0a275419f6e948a24453ed0dad
unknown <1165240284@qq.com>
2020-05-01 22:07:15 +0800

    1419.数青蛙

--- 1419.数青蛙/minNumberOfFrogs.cpp
+++ 1419.数青蛙/minNumberOfFrogs.cpp
@@ -1,27 +1,55 @@
-public int minNumberOfFrogs(String croakOfFrogs) {
+class Solution {
+public:
+    int minNumberOfFrogs(string croakOfFrogs) {
         int c,r,o,a,k;
         c = 0; r = 0; o = 0; a = 0;k = 0;
-        char []chars = croakOfFrogs.toCharArray();
+
         int res = 0;
-        for(int i = 0;i < chars.length;i++){
-            if(chars[i] == 'c'){
-                if(k > 0){k--;}else{res++;}
+        for(int i=0;i<croakOfFrogs.size();++i)
+        {
+            if(croakOfFrogs[i] == 'c')
+            {
+                if(k > 0)
+                {
+                    k--;
+                }
+                else
+                {
+                    res++;
+                }
                 c++;
-            }else if(chars[i] == 'r'){
-                c--;r++;
-            }else if(chars[i] == 'o'){
-                r--;o++;
-            }else if(chars[i] == 'a'){
-                o--;a++;
-            }else if(chars[i] == 'k'){
-                a--;k++;
             }
-            if(c < 0 || r < 0 || o < 0 || a < 0){
+            else if(croakOfFrogs[i] == 'r')
+            {
+                c--;
+                r++;
+            }
+            else if(croakOfFrogs[i] == 'o')
+            {
+                r--;
+                o++;
+            }
+            else if(croakOfFrogs[i] == 'a')
+            {
+                o--;
+                a++;
+            }
+            else if(croakOfFrogs[i] == 'k')
+            {
+                a--;
+                k++;
+            }
+            if(c < 0 || r < 0 || o < 0 || a < 0)
+            {
                 break;
             }
         }
-        if(c != 0 || r != 0 || o != 0 || a != 0){
+
+        if(c != 0 || r != 0 || o != 0 || a != 0)
+        {
             return -1;
         }
+        
         return res;
     }
+};
