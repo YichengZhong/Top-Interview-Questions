8edf2bc557a90ff4409bf0aa991cbb11d1770dbf
unknown <1165240284@qq.com>
2020-05-02 11:03:45 +0800

    面试题02.08.环路检测

---
+++ 面试题02.08.环路检测/detectCycle.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        map<ListNode *,int>m_find;
+        ListNode *temp=head;
+
+        if(NULL==head)
+        {
+            return NULL;
+        }
+
+        while(m_find.find(temp)==m_find.end())
+        {
+            if(temp->next==NULL)
+            {
+                return NULL;
+            }
+			
+            m_find[temp]=temp->val;
+            temp=temp->next;
+        }
+
+        return temp;
+    }
+};
