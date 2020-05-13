a9e67253a81bb4627a37e7522efd5c77508ab349
YichengZhong <1165240284@qq.com>
2020-05-13 21:06:32 +0800

    技术教练核心代码

--- 技术教练练习1/技术教练练习1/ReadJson.cpp
+++ 技术教练练习1/技术教练练习1/ReadJson.cpp
@@ -2,142 +2,142 @@
 
 ReadJson::ReadJson(const char str_json[])
 {
-	v_StrongPasswordSocre.clear();
-	PasswordNums = 0;
-	PasswordMS = 0;
+    v_StrongPasswordSocre.clear();
+    PasswordNums = 0;
+    PasswordMS = 0;
 
-	if (NULL == str_json)
-	{
-		Password_json = NULL;
-	}
-	else
-	{
-		Password_json = cJSON_Parse(str_json);   //创建JSON解析对象，返回JSON格式是否正确
-	}
+    if (NULL == str_json)
+    {
+        Password_json = NULL;
+    }
+    else
+    {
+        Password_json = cJSON_Parse(str_json);   //创建JSON解析对象，返回JSON格式是否正确
+    }
 }
 
 ReadJson::~ReadJson()
 {
-	if (Password_json != NULL)
-	{
-		cJSON_Delete(Password_json);//释放内存
-	}
+    if (Password_json != NULL)
+    {
+        cJSON_Delete(Password_json);//释放内存
+    }
 }
 
 void ReadJson::getPasswordInfo()
 {
-	std::vector<std::string>v_Password;
-	std::vector<int>v_PasswordSocre;
+    std::vector<std::string>v_Password;
+    std::vector<int>v_PasswordSocre;
 
-	v_Password.clear();
-	v_PasswordSocre.clear();
+    v_Password.clear();
+    v_PasswordSocre.clear();
 
-	getPasswordList(v_Password);
-	getPasswordSocreList(v_PasswordSocre);
+    getPasswordList(v_Password);
+    getPasswordSocreList(v_PasswordSocre);
 
-	if (v_Password.size() != v_PasswordSocre.size() || Password_json==NULL)
-	{
-		std::cout << "json格式异常" << std::endl;
-		return;
-	}
+    if (v_Password.size() != v_PasswordSocre.size() || Password_json==NULL)
+    {
+        std::cout << "json格式异常" << std::endl;
+        return;
+    }
 
-	DealStrongPassword myDealStrongPassword;
-	for (int i = 0; i < v_Password.size(); ++i)
-	{
-		StrongPasswordSocre temp(v_Password[i], 0, v_PasswordSocre[i], 0);
-		temp.setConsumptionSocre(myDealStrongPassword.strongPasswordChecker(temp.getPassword()));
-		int net = temp.getGainSocre() - temp.getConsumptionSocre();
-		temp.setNetSocre(net);
-		v_StrongPasswordSocre.push_back(temp);
-	}
+    DealStrongPassword myDealStrongPassword;
+    for (int i = 0; i < v_Password.size(); ++i)
+    {
+        StrongPasswordSocre temp(v_Password[i], 0, v_PasswordSocre[i], 0);
+        temp.setConsumptionSocre(myDealStrongPassword.strongPasswordChecker(temp.getPassword()));
+        int net = temp.getGainSocre() - temp.getConsumptionSocre();
+        temp.setNetSocre(net);
+        v_StrongPasswordSocre.push_back(temp);
+    }
 
-	cJSON *str_PasswordNums = cJSON_GetObjectItem(Password_json, "N");   //获取N键对应的值的信息
-	if (str_PasswordNums!=NULL && str_PasswordNums->type == cJSON_Number)
-	{
-		PasswordNums = str_PasswordNums->valueint;
-	}
-	else
-	{
-		std::cout << "json格式异常" << std::endl;
-		return;
-	}
+    cJSON *str_PasswordNums = cJSON_GetObjectItem(Password_json, "N");   //获取N键对应的值的信息
+    if (str_PasswordNums!=NULL && str_PasswordNums->type == cJSON_Number)
+    {
+        PasswordNums = str_PasswordNums->valueint;
+    }
+    else
+    {
+        std::cout << "json格式异常" << std::endl;
+        return;
+    }
 
-	cJSON *str_PasswordMS = cJSON_GetObjectItem(Password_json, "MS");   //获取MS键对应的值的信息
-	if (str_PasswordMS!=NULL && str_PasswordMS->type == cJSON_Number)
-	{
-		PasswordMS = str_PasswordMS->valueint;
-	}
-	else
-	{
-		std::cout << "json格式异常" << std::endl;
-		return;
-	}
+    cJSON *str_PasswordMS = cJSON_GetObjectItem(Password_json, "MS");   //获取MS键对应的值的信息
+    if (str_PasswordMS!=NULL && str_PasswordMS->type == cJSON_Number)
+    {
+        PasswordMS = str_PasswordMS->valueint;
+    }
+    else
+    {
+        std::cout << "json格式异常" << std::endl;
+        return;
+    }
 }
 
 void ReadJson::getPasswordList(std::vector<std::string>&v_Password)
 {
-	cJSON *str_PasswordList = cJSON_GetObjectItem(Password_json, "P"); //获取P键对应的值的信息
+    cJSON *str_PasswordList = cJSON_GetObjectItem(Password_json, "P"); //获取P键对应的值的信息
 
-	if (str_PasswordList != NULL)
-	{
-		int  array_size = cJSON_GetArraySize(str_PasswordList);
+    if (str_PasswordList != NULL)
+    {
+        int  array_size = cJSON_GetArraySize(str_PasswordList);
 
-		for (int iCnt = 0; iCnt < array_size; iCnt++)
-		{
-			cJSON * pSub = cJSON_GetArrayItem(str_PasswordList, iCnt);
-			if (NULL == pSub)
-			{
-				continue;
-			}
+        for (int iCnt = 0; iCnt < array_size; iCnt++)
+        {
+            cJSON * pSub = cJSON_GetArrayItem(str_PasswordList, iCnt);
+            if (NULL == pSub)
+            {
+                continue;
+            }
 
-			char * ivalue = pSub->valuestring;
-			v_Password.push_back(ivalue);
-			printf("str_PasswordList[%d] : %s\n", iCnt, ivalue);
-		}
-	}
-	else
-	{
-		std::cout << "json格式异常" << std::endl;
-	}
+            char * ivalue = pSub->valuestring;
+            v_Password.push_back(ivalue);
+            printf("str_PasswordList[%d] : %s\n", iCnt, ivalue);
+        }
+    }
+    else
+    {
+        std::cout << "json格式异常" << std::endl;
+    }
 }
 
 void ReadJson::getPasswordSocreList(std::vector<int>&v_PasswordSocre)
 {
-	cJSON *str_PasswordSocreList = cJSON_GetObjectItem(Password_json, "C"); //获取C键对应的值的信息
-	if (str_PasswordSocreList != NULL)
-	{
-		int  array_size = cJSON_GetArraySize(str_PasswordSocreList);
+    cJSON *str_PasswordSocreList = cJSON_GetObjectItem(Password_json, "C"); //获取C键对应的值的信息
+    if (str_PasswordSocreList != NULL)
+    {
+        int  array_size = cJSON_GetArraySize(str_PasswordSocreList);
 
-		for (int iCnt = 0; iCnt < array_size; iCnt++)
-		{
-			cJSON * pSub = cJSON_GetArrayItem(str_PasswordSocreList, iCnt);
-			if (NULL == pSub)
-			{
-				continue;
-			}
+        for (int iCnt = 0; iCnt < array_size; iCnt++)
+        {
+            cJSON * pSub = cJSON_GetArrayItem(str_PasswordSocreList, iCnt);
+            if (NULL == pSub)
+            {
+                continue;
+            }
 
-			int  ivalue = pSub->valueint;
-			v_PasswordSocre.push_back(ivalue);
-			printf("str_PasswordSocreList[%d] : %d\n", iCnt, ivalue);
-		}
-	}
-	else
-	{
-		std::cout << "json格式异常" << std::endl;
-	}
+            int  ivalue = pSub->valueint;
+            v_PasswordSocre.push_back(ivalue);
+            printf("str_PasswordSocreList[%d] : %d\n", iCnt, ivalue);
+        }
+    }
+    else
+    {
+        std::cout << "json格式异常" << std::endl;
+    }
 }
 
 std::vector<StrongPasswordSocre> ReadJson::getStrongPasswordList()
 {
-	return v_StrongPasswordSocre;
+    return v_StrongPasswordSocre;
 }
 
 int ReadJson::getPasswordNums()
 {
-	return PasswordNums;
+    return PasswordNums;
 }
 
 int ReadJson::getPasswordMS()
 {
-	return PasswordMS;
+    return PasswordMS;
 }
--- 技术教练练习1/技术教练练习1/main.cpp
+++ 技术教练练习1/技术教练练习1/main.cpp
@@ -15,95 +15,95 @@ using json = nlohmann::json;
 //计算最终结果
 void CalculateMS(std::vector<StrongPasswordSocre>v_StrongPasswordSocreVector,int PasswordNums,int PasswordMS)
 {
-	if (v_StrongPasswordSocreVector.size() == 0)
-	{
-		std::cout << PasswordMS << "-" << 0 << std::endl;
-		return;
-	}
+    if (v_StrongPasswordSocreVector.size() == 0)
+    {
+        std::cout << PasswordMS << "-" << 0 << std::endl;
+        return;
+    }
 
-	std::sort(v_StrongPasswordSocreVector.begin(), v_StrongPasswordSocreVector.end(), StrongPasswordSocre::selfcmp);
+    std::sort(v_StrongPasswordSocreVector.begin(), v_StrongPasswordSocreVector.end(), StrongPasswordSocre::selfcmp);
 
-	int outPasswordNums = 0, outPasswordMS = PasswordMS;
+    int outPasswordNums = 0, outPasswordMS = PasswordMS;
 
-	int outPasswordCheckNums = 0;
-	for (int i = 0; i < std::min((int)v_StrongPasswordSocreVector.size(), PasswordNums); ++i)
-	{
-		if (v_StrongPasswordSocreVector[i].getNetSocre() > 0)
-		{
-			outPasswordMS = outPasswordMS + v_StrongPasswordSocreVector[i].getNetSocre();
-			outPasswordCheckNums++;
-		}
-		else
-		{
-			break;
-		}
-	}
+    int outPasswordCheckNums = 0;
+    for (int i = 0; i < std::min((int)v_StrongPasswordSocreVector.size(), PasswordNums); ++i)
+    {
+        if (v_StrongPasswordSocreVector[i].getNetSocre() > 0)
+        {
+            outPasswordMS = outPasswordMS + v_StrongPasswordSocreVector[i].getNetSocre();
+            outPasswordCheckNums++;
+        }
+        else
+        {
+            break;
+        }
+    }
 
-	std::cout << outPasswordMS << "-" << outPasswordCheckNums << std::endl;
+    std::cout << outPasswordMS << "-" << outPasswordCheckNums << std::endl;
 }
 
 void test1()
 {
-	StrongPasswordSocre temp1("!@#",0,3,0);
-	StrongPasswordSocre temp2("abcdEF",0,2,0);
-	StrongPasswordSocre temp3("ab1dEF", 0, 2, 0);
+    StrongPasswordSocre temp1("!@#",0,3,0);
+    StrongPasswordSocre temp2("abcdEF",0,2,0);
+    StrongPasswordSocre temp3("ab1dEF", 0, 2, 0);
 
-	StrongPasswordCheck myDealStrongPassword;
-	int net = 0;
+    StrongPasswordCheck myDealStrongPassword;
+    int net = 0;
 
-	temp1.setConsumptionSocre(myDealStrongPassword.strongPasswordChecker(temp1.getPassword()));
-	net = temp1.getGainSocre() - temp1.getConsumptionSocre();
-	temp1.setNetSocre(net);
+    temp1.setConsumptionSocre(myDealStrongPassword.strongPasswordChecker(temp1.getPassword()));
+    net = temp1.getGainSocre() - temp1.getConsumptionSocre();
+    temp1.setNetSocre(net);
 
-	temp2.setConsumptionSocre(myDealStrongPassword.strongPasswordChecker(temp2.getPassword()));
-	net = temp2.getGainSocre() - temp2.getConsumptionSocre();
-	temp2.setNetSocre(net);
+    temp2.setConsumptionSocre(myDealStrongPassword.strongPasswordChecker(temp2.getPassword()));
+    net = temp2.getGainSocre() - temp2.getConsumptionSocre();
+    temp2.setNetSocre(net);
 
-	temp3.setConsumptionSocre(myDealStrongPassword.strongPasswordChecker(temp3.getPassword()));
-	net = temp3.getGainSocre() - temp3.getConsumptionSocre();
-	temp3.setNetSocre(net);
+    temp3.setConsumptionSocre(myDealStrongPassword.strongPasswordChecker(temp3.getPassword()));
+    net = temp3.getGainSocre() - temp3.getConsumptionSocre();
+    temp3.setNetSocre(net);
 
-	std::vector<StrongPasswordSocre>v_StrongPasswordSocreVector;
-	v_StrongPasswordSocreVector.clear();
+    std::vector<StrongPasswordSocre>v_StrongPasswordSocreVector;
+    v_StrongPasswordSocreVector.clear();
 
-	v_StrongPasswordSocreVector.push_back(temp1);
-	v_StrongPasswordSocreVector.push_back(temp2);
-	v_StrongPasswordSocreVector.push_back(temp3);
+    v_StrongPasswordSocreVector.push_back(temp1);
+    v_StrongPasswordSocreVector.push_back(temp2);
+    v_StrongPasswordSocreVector.push_back(temp3);
 
-	CalculateMS(v_StrongPasswordSocreVector, 2, 1);
+    CalculateMS(v_StrongPasswordSocreVector, 2, 1);
 }
 
 void test2()
 {
-	std::ifstream InJsonFile("file.json"); //可以读取txt、json等文件
+    std::ifstream InJsonFile("file.json"); //可以读取txt、json等文件
 
-	if (!InJsonFile.is_open())
-	{
-		std::cout << "文件打开失败！" << std::endl;
-	}
+    if (!InJsonFile.is_open())
+    {
+        std::cout << "文件打开失败！" << std::endl;
+    }
 
-	std::string str((std::istreambuf_iterator<char>(InJsonFile)),std::istreambuf_iterator<char>());
-	std::cout << str << std::endl;
+    std::string str((std::istreambuf_iterator<char>(InJsonFile)),std::istreambuf_iterator<char>());
+    std::cout << str << std::endl;
 
-	//char str_json[] = "{\"P\":[\"123456\",\"abcdEF\"],\"C\":[3,2],\"N\":2,\"MS\":1}";
-	const char *str_json = str.c_str();
-	ReadJson myReadJson(str_json);
-	myReadJson.getPasswordInfo();
+    //char str_json[] = "{\"P\":[\"123456\",\"abcdEF\"],\"C\":[3,2],\"N\":2,\"MS\":1}";
+    const char *str_json = str.c_str();
+    ReadJson myReadJson(str_json);
+    myReadJson.getPasswordInfo();
 
-	std::vector<StrongPasswordSocre>v_StrongPasswordSocreVector;
-	v_StrongPasswordSocreVector.clear();
-	v_StrongPasswordSocreVector = myReadJson.getStrongPasswordList();
-	int PasswordNums= myReadJson.getPasswordNums();
-	int PasswordMS = myReadJson.getPasswordMS();
+    std::vector<StrongPasswordSocre>v_StrongPasswordSocreVector;
+    v_StrongPasswordSocreVector.clear();
+    v_StrongPasswordSocreVector = myReadJson.getStrongPasswordList();
+    int PasswordNums= myReadJson.getPasswordNums();
+    int PasswordMS = myReadJson.getPasswordMS();
 
-	CalculateMS(v_StrongPasswordSocreVector, PasswordNums, PasswordMS);
+    CalculateMS(v_StrongPasswordSocreVector, PasswordNums, PasswordMS);
 }
 
 int main()
 {
-	//test1();
-	test2();
-	//test3();
-	//test4();
-	return 0;
+    //test1();
+    test2();
+    //test3();
+    //test4();
+    return 0;
 }
