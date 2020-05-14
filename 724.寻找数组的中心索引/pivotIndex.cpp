4c854f208a8983d4b267420cc234fac5d7798871
YichengZhong <1165240284@qq.com>
2020-05-14 21:40:27 +0800

    724.寻找数组的中心索引

---
+++ 724.寻找数组的中心索引/pivotIndex.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+            int sumLeft=0;//索引左边数组元素的和
+            int sumTotal=0;//数组所有元素的和
+        for(int i=0;i<nums.size();i++)
+            sumTotal+=nums[i];
+        for(int j=0;j<nums.size();j++)
+        {
+          if(sumLeft*2==sumTotal-nums[j])
+              return j;
+
+            sumLeft+=nums[j];
+        }
+        return -1;
+    }
+};
