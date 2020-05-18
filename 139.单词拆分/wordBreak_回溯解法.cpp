55529a09ea5857b00c73331ff1a8c1eb6ebb863f
YichengZhong <1165240284@qq.com>
2020-05-18 21:46:16 +0800

    139记忆化回溯法

--- 139.单词拆分/wordBreak_回溯解法.cpp
+++ 139.单词拆分/wordBreak_回溯解法.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    map<int,bool>m_str;
     bool wordBreak(string s, vector<string>& wordDict) {
         return word_Break_help(s,wordDict, 0);
     }
@@ -11,15 +12,20 @@ public:
             return true;
         }
 
+        if(m_str.find(start) != m_str.end())
+        {
+            return m_str[start];
+        }
+
         for(int end=start+1;end<=s.size();++end)
         {
             if (wordindict(s.substr(start, end-start),wordDict)&& word_Break_help(s, wordDict,end)) 
             {
-                return true;
+                return m_str[start] = true;
             }
         }
 
-        return false;
+        return m_str[start]=false;
     }
 
     bool wordindict(string s,vector<string>& wordDict)
