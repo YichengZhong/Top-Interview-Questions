880e739812d4e6fc2a585fd45755bd27eb52a8f3
YichengZhong <1165240284@qq.com>
2020-05-20 20:56:50 +0800

    最长无重复的子串

--- 3.无重复字符的最长子串/lengthOfLongestSubstring.cpp
+++ 3.无重复字符的最长子串/lengthOfLongestSubstring.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-            int length=s.length();
-        //字符串长度为0、1时直接返回数字，双指针没有地方指。
-        if(!length)
-            return 0;
-        if(length==1)
-            return 1;
-        //头指针p，最大长度mlen至少为1。
-        int p=0,mlen=1;
-        //尾指针end
-        for(int end=1;end<length;end++){
-            for(int aim=p;aim<end;aim++){
-                //情况1
-                if(s[aim]==s[end]){
-                    //更新头指针p
-                    p=aim+1;
-                    //更新最大长度mlen
-                    if(mlen<(end-aim))
-                        mlen=end-aim;
-                    break;
-                }
+           if(s.size() == 0)
+               return 0;
+        unordered_set<char> lookup;
+        int maxStr = 0;
+        int left = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            while (lookup.find(s[i]) != lookup.end())
+            {
+                lookup.erase(s[left]);
+                left ++;
             }
-            //情况2。要算上尾指针所以+1。
-            if(mlen<end-p+1)
-                mlen=end-p+1;
+            maxStr = max(maxStr,i-left+1);
+            lookup.insert(s[i]);
         }
-        return mlen;
+        return maxStr;
+
         
     }
 };
