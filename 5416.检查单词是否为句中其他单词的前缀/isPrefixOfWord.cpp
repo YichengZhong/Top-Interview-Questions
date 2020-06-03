e07c2553a450b5308a2824d48508ffa818c510b1
YichengZhong <1165240284@qq.com>
2020-06-03 19:57:26 +0800

    1455.检查单词是否为句中其他单词的前缀

---
+++ 5416.检查单词是否为句中其他单词的前缀/isPrefixOfWord.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) 
+    {
+        istringstream ss(sentence);
+        string str;
+        for (int i = 1; ss >> str; i ++)
+        {
+            if (str.find(searchWord) == 0) 
+                return i;
+        }
+            
+        return -1;
+
+    }
+};
