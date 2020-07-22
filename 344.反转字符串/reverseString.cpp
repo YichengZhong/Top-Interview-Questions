c7a156667923ca323b93db06766509db8914f413
YichengZhong <1165240284@qq.com>
2020-07-22 22:41:37 +0800

    344.反转字符串

---
+++ 344.反转字符串/reverseString.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s)
+    {
+        for(int i = 0,j = s.size()-1;i <= j ;i++,j--)
+        {
+           char temp = s[i];
+           s[i] = s[j];
+           s[j] = temp;
+        }
+    }
+};
