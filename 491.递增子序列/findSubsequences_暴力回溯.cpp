c83e343f83279697976b6a76863b13e3dd82b3db
YichengZhong <1165240284@qq.com>
2020-08-25 21:23:21 +0800

    491.递增子序列_暴力回溯法

---
+++ 491.递增子序列/findSubsequences_暴力回溯.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> v_v_out;
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        v_v_out.clear();
+        vector<int>v_temp;
+        help(nums,v_temp,0);
+
+        return v_v_out;
+    }
+
+    void help(vector<int>& nums,vector<int>v_temp,int index)
+    {
+        if(v_temp.size()>1 && find(v_v_out.begin(),v_v_out.end(),v_temp)==v_v_out.end())
+        {
+            v_v_out.push_back(v_temp);
+        }
+        
+        for(int i=index;i<nums.size();++i)
+        {
+            if(v_temp.size()<1)
+            {
+                v_temp.push_back(nums[i]);
+                help(nums,v_temp,i+1);
+                v_temp.pop_back();
+            }
+            else if(v_temp[v_temp.size()-1]<=nums[i])
+            {
+                v_temp.push_back(nums[i]);
+                help(nums,v_temp,i+1);
+                v_temp.pop_back();
+            }
+        }
+    }
+};
