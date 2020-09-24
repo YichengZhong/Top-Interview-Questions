417cb5b93aed6e4f550f68a46abab3f3a12fa85f
YichengZhong <1165240284@qq.com>
2020-09-24 22:02:50 +0800

    56.合并区间

--- 56.合并区间/merge.cpp
+++ 56.合并区间/merge.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) 
-    {
-        sort(intervals.begin(), intervals.end(), cmp);
-        if(intervals.size() == 1)
-            return intervals;
-        vector<vector<int>> res;
-        int i = 0;
-        while(i < intervals.size())
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.size() == 0) 
         {
-            int start = intervals[i][0], end = intervals[i][1];
-            int j = i+1;
-            while(j < intervals.size() && intervals[j][0] <= end)
-            {
-                if(end < intervals[j][1])
-                    end = intervals[j][1];
-                j++;
-            }
-            vector<int>temp;
-            temp.push_back(start);
-            temp.push_back(end);
-            res.push_back(temp);
-            i = j;
+            return {};
         }
-        return res;
-
-    }
-
-    static bool cmp(vector<int> a, vector<int> b)
-    {
-        return a[0] < b[0];
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> merged;
+        for (int i = 0; i < intervals.size(); ++i) 
+        {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (!merged.size() || merged.back()[1] < L) 
+            {
+                merged.push_back({L, R});
+            }
+            else {
+                merged.back()[1] = max(merged.back()[1], R);
+            }
+        }
+        return merged;
     }
 };
