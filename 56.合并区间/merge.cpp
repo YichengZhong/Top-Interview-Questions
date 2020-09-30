6a8e127792b7888b9a27d5e1c018d741a72949f3
YichengZhong <1165240284@qq.com>
2020-09-30 19:42:09 +0800

    744.寻找比目标字母大的最小字母

--- 56.合并区间/merge.cpp
+++ 56.合并区间/merge.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) 
-	{
+    {
         if (intervals.size() == 0) 
         {
             return {};
@@ -16,7 +16,7 @@ public:
                 merged.push_back({L, R});
             }
             else 
-			{
+            {
                 merged.back()[1] = max(merged.back()[1], R);
             }
         }
