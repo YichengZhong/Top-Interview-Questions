c6650da789d8d3a40c95bf15cc197a232c5d5905
YichengZhong <1165240284@qq.com>
2021-01-13 19:54:39 +0800

    59.螺旋矩阵II

---
+++ 59.螺旋矩阵II/generateMatrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int l = 0, r = n - 1, t = 0, b = n - 1;
+        vector<vector<int>>mat(n,vector<int>(n,0));
+        int num = 1, tar = n * n;
+        while(num <= tar)
+        {
+            for(int i = l; i <= r; i++) mat[t][i] = num++; // left to right.
+            t++;
+            for(int i = t; i <= b; i++) mat[i][r] = num++; // top to bottom.
+            r--;
+            for(int i = r; i >= l; i--) mat[b][i] = num++; // right to left.
+            b--;
+            for(int i = b; i >= t; i--) mat[i][l] = num++; // bottom to top.
+            l++;
+        }
+        return mat;
+
+    }
+};
