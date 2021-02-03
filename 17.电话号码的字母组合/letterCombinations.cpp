e2e3f91e6a553ce64dbb7ee60556bb516e20005f
YichengZhong <1165240284@qq.com>
2021-02-03 20:33:44 +0800

    17.电话号码的字母组合_二刷

--- 17.电话号码的字母组合/letterCombinations.cpp
+++ 17.电话号码的字母组合/letterCombinations.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     std::unordered_map<char, std::string> m;
-
-void InitMap() {
-  m['2'] = "abc";
-  m['3'] = "def";
-  m['4'] = "ghi";
-  m['5'] = "jkl";
-  m['6'] = "mno";
-  m['7'] = "pqrs";
-  m['8'] = "tuv";
-  m['9'] = "wxyz";
-}
-void Helper(const std::string &digits, string &phone_number, std::vector<std::string> &result) 
-{
-  if (phone_number.size() == digits.size())
-   {
-    result.push_back(phone_number);
-  } else 
-  {
-    for (char c: m[digits[phone_number.size()]]) 
+    vector<string>v_out;
+    vector<string> letterCombinations(string digits) 
     {
-      phone_number.push_back(c);
-      Helper(digits, phone_number, result);
-      phone_number.pop_back();
+        if(digits=="") return {};
+        
+        m['2'] = "abc";
+        m['3'] = "def";
+        m['4'] = "ghi";
+        m['5'] = "jkl";
+        m['6'] = "mno";
+        m['7'] = "pqrs";
+        m['8'] = "tuv";
+        m['9'] = "wxyz";
+
+        helpDFS(0,digits,"");
+
+        return v_out;
+
     }
-  }
-}
-  
-vector<string> letterCombinations(string digits) {
-  if (digits.empty()) return {};
-  InitMap();
-  std::string phone_number;
-  std::vector<std::string> result;
-  Helper(digits, phone_number, result);
-  return result;
-}
+
+    void helpDFS(int index,string digits,string tmp)
+    {
+        if(index>=digits.size())
+        {
+            v_out.push_back(tmp);
+            return ;
+        }
+
+        for(int i=0;i<m[digits[index]].size();++i)
+        {
+            helpDFS(index+1,digits,tmp+m[digits[index]][i]);
+        }
+    }
 };
