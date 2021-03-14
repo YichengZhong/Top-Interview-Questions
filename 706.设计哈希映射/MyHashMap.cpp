20d9afb4327b8498ffdd63e6d51ecd4c898974eb
YichengZhong <1165240284@qq.com>
2021-03-14 09:51:07 +0800

    706.设计哈希映射_不用内建数据结构

--- 706.设计哈希映射/MyHashMap.cpp
+++ 706.设计哈希映射/MyHashMap.cpp
@@ -1,30 +1,61 @@
+
+
 class MyHashMap {
+private:
+    constexpr static int maxSize = 1021;
+    // pair<int,int> 是pair<key, value>
+    vector<list<pair<int, int>>> v;
+
+    int GetIndex(int x)
+    {
+        return x % maxSize;
+    }
 public:
     /** Initialize your data structure here. */
-    map<int,int>inermap;
-    MyHashMap() {
-        
+    MyHashMap() : v(maxSize){
 
     }
     
     /** value will always be non-negative. */
     void put(int key, int value) {
-        inermap[key]=value;
-
+        int i = GetIndex(key);
+        for (auto iter = v[i].begin(); iter != v[i].end(); ++iter)
+        {
+            // 已存在需要删除
+            if ((*iter).first == key)
+            {
+                v[i].erase(iter);
+                break;
+            }
+        }
+        v[i].emplace_back(key, value);
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-        if(inermap.find(key)==inermap.end()) return -1;
-        return inermap[key];
+        int i = GetIndex(key);
+        for (auto iter = v[i].begin(); iter != v[i].end(); ++iter)
+        {
+            // 找到则返回
+            if ((*iter).first == key)
+            {
+                return (*iter).second;
+            }
+        }
+        return -1;
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-        auto keyiter=inermap.find(key);
-        if(keyiter!=inermap.end())
+        int i = GetIndex(key);
+        for (auto iter = v[i].begin(); iter != v[i].end(); ++iter)
         {
-            inermap.erase(keyiter);
+            if ((*iter).first == key)
+            {
+                // 删除存在的结点
+                v[i].erase(iter);
+                return;
+            }
         }
     }
 };
