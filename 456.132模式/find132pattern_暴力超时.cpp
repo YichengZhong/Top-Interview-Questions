ade8a16fdcacee7211b06b5252d473c5a2c93f09
YichengZhong <1165240284@qq.com>
2021-03-24 21:31:51 +0800

    456.132模式_暴力超时

---
+++ 456.132模式/find132pattern_暴力超时.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        //先来暴力
+        for(int i=0;i<nums.size();++i)
+        {
+            for(int j=i+1;j<nums.size();++j)
+            {
+                for(int k=j+1;k<nums.size();++k)
+                {
+                    if(nums[i]<nums[k] && nums[k]<nums[j]) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
