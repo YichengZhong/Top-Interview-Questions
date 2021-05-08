ebeef7375a3238cef59551e43c1a39b490dfa6d3
YichengZhong <1165240284@qq.com>
2021-05-08 23:01:12 +0800

    1720.解码异或后的数组

---
+++ 1720.解码异或后的数组/decode.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        int n = encoded.size() + 1;
+        vector<int> arr(n);
+        arr[0] = first;
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i - 1] ^ encoded[i - 1];
+        }
+        return arr;
+    }
+};
