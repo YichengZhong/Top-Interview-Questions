92de2bfd51708cc370e25a0ac2ea32ffce19a3ad
YichengZhong <1165240284@qq.com>
2021-05-09 19:37:59 +0800

    剑指Offer40.最小的k个数

---
+++ 剑指Offer40.最小的k个数/getLeastNumbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        vector<int> vec(k, 0);
+        sort(arr.begin(), arr.end());
+        for (int i = 0; i < k; ++i) 
+        {
+            vec[i] = arr[i];
+        }
+        return vec;
+    }
+};
