ef8e8041a21707025e58506097e7ffdd977849eb
YichengZhong <1165240284@qq.com>
2021-05-28 20:50:17 +0800

    477.汉明距离总和

---
+++ 477.汉明距离总和/totalHammingDistance.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int> &nums) {
+        int ans = 0, n = nums.size();
+        for (int i = 0; i < 30; ++i)
+        {
+            int c = 0;
+            for (int val : nums) 
+            {
+                c += (val >> i) & 1;
+            }
+            ans += c * (n - c);
+        }
+        return ans;
+    }
+};
