baa6e03cbeadf9c899e607059841cc2496de7627
YichengZhong <1165240284@qq.com>
2021-06-02 20:44:21 +0800

    523.连续的子数组和

---
+++ 523.连续的子数组和/checkSubarraySum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        //前缀和法
+        int m = nums.size();
+        if (m < 2) return false;
+
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int remainder = 0;
+        for (int i = 0; i < m; i++) 
+        {
+            remainder = (remainder + nums[i]) % k;
+            if (mp.count(remainder)) 
+            {
+                int prevIndex = mp[remainder];
+                if (i - prevIndex >= 2) 
+                {
+                    return true;
+                }
+            } else 
+            {
+                mp[remainder] = i;
+            }
+        }
+        return false;
+    }
+};
