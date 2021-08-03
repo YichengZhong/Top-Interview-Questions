2fba121c20f3ac24e0b04fea3b67316902cfcd29
unknown <1165240284@qq.com>
2021-08-03 21:24:37 +0800

    207.课程表

---
+++ 207.课程表/canFinish_迭代.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    vector<vector<int>> edges;
+    vector<int> indeg;
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        edges.resize(numCourses);
+        indeg.resize(numCourses);
+        for (const auto& info: prerequisites) {
+            //计算相连的边
+            edges[info[1]].push_back(info[0]);
+            //计算出度
+            ++indeg[info[0]];
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (indeg[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int visited = 0;
+        while (!q.empty()) {
+            ++visited;
+            int u = q.front();
+            q.pop();
+            for (int v: edges[u]) {
+                --indeg[v];
+                if (indeg[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+        return visited == numCourses;
+    }
+};
