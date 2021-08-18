b66abd0bff722cb3fcdb0636b4d6bfd0337c84e9
unknown <1165240284@qq.com>
2021-08-18 20:21:20 +0800

    526.优美的排列

---
+++ 回溯/526.优美的排列/countArrangement.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> match;
+    vector<int> vis;
+    int num;
+
+    int countArrangement(int n) {
+        vis.resize(n + 1);
+        match.resize(n + 1);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i % j == 0 || j % i == 0) {
+                    match[i].push_back(j);
+                }
+            }
+        }
+        backtrack(1, n);
+        return num;
+    }
+
+    void backtrack(int index, int n) {
+        if (index == n + 1) {
+            num++;
+            return;
+        }
+        for (auto &x : match[index]) {
+            if (!vis[x]) {
+                vis[x] = true;
+                backtrack(index + 1, n);
+                vis[x] = false;
+            }
+        }
+    }
+};
