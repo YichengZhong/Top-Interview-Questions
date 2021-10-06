244999025b884eda83fa77fd7bec69b1f2cbfecb
YichengZhong <1165240284@qq.com>
2021-10-06 21:22:54 +0800

    414.第三大的数

---
+++ 414.第三大的数/thirdMax.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int thirdMax(vector<int> &nums) {
+        set<int> s;
+        for (int num : nums) {
+            s.insert(num);
+            if (s.size() > 3) {
+                s.erase(s.begin());
+            }
+        }
+        
+        return s.size() == 3 ? *s.begin() : *s.rbegin();
+    }
+};
