200050e3f2b7d67ad2a042cfe0cc29e064252e90
YichengZhong <1165240284@qq.com>
2021-10-21 21:46:51 +0800

    236.二叉树的最近公共祖先

---
+++ 236.二叉树的最近公共祖先/lowestCommonAncestor.cpp
@@ -0,0 +1,22 @@
+、/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || root == p || root == q) return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if(left == nullptr && right == nullptr) return nullptr; // 1.
+        if(left == nullptr) return right; // 3.
+        if(right == nullptr) return left; // 4.
+        
+        return root; // 2. if(left != null and right != null)
+    }
+};
