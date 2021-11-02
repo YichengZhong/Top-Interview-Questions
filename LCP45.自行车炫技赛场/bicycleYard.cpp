c801bcbc17a8a9d553d8a9b3940d99aa9325c913
YichengZhong <1165240284@qq.com>
2021-11-02 20:56:19 +0800

    剑指OfferII053.二叉搜索树中的中序后继

--- LCP45.自行车炫技赛场/bicycleYard.cpp
+++ LCP45.自行车炫技赛场/bicycleYard.cpp
@@ -1,30 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    int vis[110][110][105],ab[4][2] = {0,-1,1,0,0,1,-1,0},n,m;
-    void dfs(int x,int y,int v, vector<vector<int>>& t, vector<vector<int>>& o){
-        for (int i = 0; i < 4; i ++){
-            int xx = x + ab[i][0], yy = y + ab[i][1];
-            if (xx >= 0 && xx < n && yy >= 0 && yy < m){
-                int vv = t[x][y] - t[xx][yy] - o[xx][yy] + v;
-                if (vv >= 1&&!vis[xx][yy][vv]){
-                    vis[xx][yy][vv] = 1;
-                    dfs(xx,yy,vv,t,o);
-                }
+    vector<pair<int,TreeNode*>>mid_node;
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        helpDFS(root);
+
+        sort(mid_node.begin(),mid_node.end());
+        for(int i=0;i<mid_node.size();++i)
+        {
+            if(mid_node[i].first>p->val)
+            {
+                return mid_node[i].second;
             }
         }
+
+        return NULL;        
     }
-    vector<vector<int>> bicycleYard(vector<int>& p, vector<vector<int>>& t, vector<vector<int>>& o) {
-        vector<vector<int>> res;
-        n = t.size(),m = t[0].size();
-        vis[p[0]][p[1]][1] = 1;
-        dfs(p[0],p[1],1,t,o);
-        for (int i = 0; i < n; i ++){
-            for (int j = 0; j < m; j ++){
-                if (vis[i][j][1]&&!(i == p[0] && j == p[1])){
-                    res.push_back({i,j});
-                }
-            }
-        }
-        return res;
+
+    void helpDFS(TreeNode* root)
+    {
+        if(root==NULL)
+            return ;
+        
+        helpDFS(root->left);
+        pair<int,TreeNode*> p2(root->val,root);
+        mid_node.push_back(p2);
+        helpDFS(root->right);
     }
 };
